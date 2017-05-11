aeb84825cec14b3d8330421ad1f4220121c18ceb
williamayerst <william@ayerst.net>
2017-05-11 15:36:14 +0100

    initial

---
+++ New-EncryptionContext.ps1
@@ -0,0 +1,30 @@
+﻿
+## CREATE KEY
+
+Function New-EncryptKey
+{
+$AESKey = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($AESKey)
+Set-Content .\PassEncryptKey.key $AESKey
+}
+
+## ENCRYPT PASS WITH KEY
+
+Function Get-EncryptedPassword
+{
+  param (
+    [Parameter(Mandatory=$true,HelpMessage='Please specify password in clear text')][ValidateNotNullOrEmpty()][String]$Password
+  )
+  $secPw = ConvertTo-SecureString -AsPlainText $Password -Force
+  $AESKey = Get-content .\PassEncryptKey.key
+  $Encryptedpassword = $secPw | ConvertFrom-SecureString -Key $AESKey
+  $Encryptedpassword
+}
+
+if (!(test-path .\PassEncryptKey.key)) {New-EncryptKey}
+
+$plain = Read-host "Enter string to encrypt" 
+Get-EncryptedPassword $plain | clip
+Write-host "encrypted PW now in clipboard"
+
+# 76492d1116743f0423413b16050a5345MgB8AGoAUQBYAHoAcgB6AHQAUQBIAGYAcwBpAGQAUwBUAEgAaAAyAEIAbgBWAEEAPQA9AHwAMgBlAGEAOQA2AGEAZAA4ADcAYwA0ADgANwAzAGQAMQBiADgAMwAzADAAYwAzAGIAZgBhAGIANgBjAGIANQBmAGUAMgBhADgAZQBiADcAMwA4ADMAMgBhADEAMwBhADgAYwBjADAANQAyAGUAMwA1ADgANABhADgAYwBjADEAMgA=

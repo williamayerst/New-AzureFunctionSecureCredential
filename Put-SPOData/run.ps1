636173835c5dd317319f24e0e8aa591e31aaf012
williamayerst <william@ayerst.net>
2017-05-11 17:08:40 +0100

    fix

--- Put-SPOData/run.ps1
+++ Put-SPOData/run.ps1
@@ -1,26 +1,30 @@
 ﻿write-output "Adding in DLL's"
-
-Add-Type -Path "D:\home\site\wwwroot\TimerTriggerPowerShell1\bin\Microsoft.SharePoint.Client.dll"   
-Add-Type -Path "D:\home\site\wwwroot\TimerTriggerPowerShell1\bin\Microsoft.SharePoint.Client.Runtime.dll"   
+Add-Type -Path "D:\home\site\wwwroot\Put-SPOData\bin\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "D:\home\site\wwwroot\Put-SPOData\bin\Microsoft.SharePoint.Client.Runtime.dll"
 
 write-output "Pulling data from App Settings via Environment variables..."
-
 $username = $env:SPUsername
 $password = $env:SPUserpass
-$Url = "https://ayerst.sharepoint.com"
-
-write-output "username is: $username"
-write-output "encrypted pw is $password"
-write-output "URL is $password"
+$url = $env:SPUrl
+$listTitle = $env:SPListTitle
 
 write-output "Setting Encryption Key Data..."
 $keypath = "D:\home\site\wwwroot\Put-SPOData\PassEncryptKey.key"
-
-test-path $keypath
 
 write-output "Converting App Setting encrypted PW to PSCredential..."
 $secpassword = $password | ConvertTo-SecureString -key (Get-content $keypath)
 $credential = New-Object System.Management.Automation.PSCredential ($username, $secpassword)
 
+write-output "Setting SPO Context"
+$context =  New-Object Microsoft.SharePoint.Client.ClientContext($Url)
 
-write-output "Got my credential for $($credential.username)!"
+write-output "Setting SPO Creds"
+$context.Credentials = $credential
+
+write-output "Doing Magical Sharepoint Things...."
+$list = $context.Web.Lists.GetByTitle($listTitle)
+$context.Load($list.RootFolder)
+$context.ExecuteQuery()
+$listUrl = $list.RootFolder.ServerRelativeUrl
+
+write-output "List Relative Url: " $listUrl

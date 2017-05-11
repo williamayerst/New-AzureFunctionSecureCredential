9aea2dd40d1e62e24248a8b7ca7801136aab50e3
williamayerst <william@ayerst.net>
2017-05-11 18:17:10 +0100

    fix

--- Put-SPOData/run.ps1
+++ Put-SPOData/run.ps1
@@ -1,5 +1,5 @@
 ######################################################
-write-output "Creating Sub-Functions"
+write-output "Creating Sub-Functions for De/Encrypting via KeyFile"
 # see : https://gist.github.com/ctigeek/2a56648b923d198a6e60
 ######################################################
 function Create-AesManagedObject($key, $IV) {
@@ -53,26 +53,24 @@ function Decrypt-String($key, $encryptedStringWithIV) {
   [System.Text.Encoding]::UTF8.GetString($unencryptedData).Trim([char]0)
 }
 ######################################################
-write-output "Adding in DLL's"
+write-output "Adding in DLL's for Sharepoint"
 Add-Type -Path "D:\home\site\wwwroot\Put-SPOData\bin\Microsoft.SharePoint.Client.dll"
 Add-Type -Path "D:\home\site\wwwroot\Put-SPOData\bin\Microsoft.SharePoint.Client.Runtime.dll"
-
 ######################################################
 write-output "Setting Encryption Key Data..."
 $aeskeypath = "D:\home\site\wwwroot\Put-SPOData\AES.key"
 if (test-path $aeskeypath) {write-output "...key found"} else {write-output "...key not found" ; throw }
-
 ######################################################
 write-output "Converting App Setting encrypted PW to PSCredential..."
 $secpassword = $(ConvertTo-SecureString -string $(Decrypt-String $(get-content $aeskeypath) $env:SPUserpass) -AsPlainText -Force)
 $credential = New-Object  Microsoft.SharePoint.Client.SharePointOnlineCredentials ($env:SPUsername, $secpassword)
-
 ######################################################
-write-output "Setting SPO Context"
-$context =  New-Object Microsoft.SharePoint.Client.ClientContext($env:SPUrl)
-write-output "Setting SPO Creds"
-$context.Credentials = $credential
 write-output "Doing Magical Sharepoint Things...."
+write-output "...Setting SPO Context"
+$context =  New-Object Microsoft.SharePoint.Client.ClientContext($env:SPUrl)
+write-output "...Setting SPO Creds"
+$context.Credentials = $credential
+write-output "...some other stuff, I guess???"
 $list = $context.Web.Lists.GetByTitle($env:SPListTitle)
 $context.Load($list.RootFolder)
 $context.ExecuteQuery()

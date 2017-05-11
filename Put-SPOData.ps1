f52d1effddc96459bb9a3adbb095997fe53517bf
williamayerst <william@ayerst.net>
2017-05-11 16:09:29 +0100

    function.json, subfolders 2

---
+++ Put-SPOData.ps1
@@ -0,0 +1,17 @@
+﻿write-host "Pulling data from App Settings via Environment variables..."
+$username = $env:SPUsername
+$password = $env:SPUserpass
+write-host "username is: $username"
+write-host "encrypted pw is $password"
+
+#write-host "Setting Encryption Key Data..."
+#$keypath = "D:\home\site\wwwroot\PassEncryptKey.key"
+
+#test-path $keypath
+
+#write-host "Converting App Setting encrypted PW to PSCredential..."
+#$secpassword = $password | ConvertTo-SecureString -key (Get-content $keypath)
+#$credential = New-Object System.Management.Automation.PSCredential ($username, $secpassword)
+#
+#
+#write-host "Got my credential for $($credential.username)!"
